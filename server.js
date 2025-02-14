812102de5207aa3ec9fbba41a2b64a8c3e7bf664
ssufhbfndk <ssufhbfndk@gmail.com>
2025-02-14 17:09:09 +0500

    CORS middleware added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: `https://yt-view-front.vercel.app/`, credentials: true }));
 app.use(sessionMiddleware);
 
 // Test Route
