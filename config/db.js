d9de8098e54205053367e0a049aaddefb657fc61
ssufhbfndk <ssufhbfndk@gmail.com>
2025-02-15 00:56:49 +0500

    db connect

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config(); // Load environment variables
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "",
   user: process.env.DB_USER || "",
-  password: process.env.DB_PASS || "",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "",
 });
 
