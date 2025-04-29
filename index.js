15d577fd870ab57621f0093bf2e045829c33d6de
ssufhbfndk <ssufhbfndk@gmail.com>
2025-04-29 13:01:03 +0500

    Added cluster module to Node.js server for multi-core handling

---
+++ index.js
@@ -0,0 +1,18 @@
+const cluster = require('cluster');
+const os = require('os');
+
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master ${process.pid} running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died, restarting...`);
+    cluster.fork();
+  });
+} else {
+  require('./server'); // Tumhara pura server code
+}
