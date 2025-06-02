3e49b109dc0a71151d5338fa776a6850fe576dbf
ssufhbfndk <ssufhbfndk@gmail.com>
2025-06-02 16:20:49 +0500

    � Updated fetchNewOrders job to run every 10 mins with error handling

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "myapp-cluster",
+      name: "ythub",
       script: "./index.js",
-      instances: 1,          // sirf 1 instance of index.js, kyunki cluster aap khud kar rahe hain
-      exec_mode: "fork",     // fork mode, cluster mode PM2 ko mat do yahan
-      watch: true,           // file changes pe restart karna hai
-      ignore_watch: ["node_modules", "logs"],  // in folders ko watch nahi karna
+      instances: 1,          
+      exec_mode: "fork",     
+      watch: true,           
+      ignore_watch: ["node_modules", "logs"], 
       autorestart: true,
       max_restarts: 10,
-      restart_delay: 3000    // 3 sec delay agar restart hota hai
+      restart_delay: 3000    
     }
   ]
 };
