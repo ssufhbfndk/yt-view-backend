1f790cf2cc989486a425c066697151ec15910b97
zain khan <ssufhbfndk@gmail.com>
2026-09-25 11:33:32 +0500

    app update 1.4.2

--- install-app.js
+++ install-app.js
@@ -12,10 +12,10 @@ app.get("/download-apk", (req, res) => {
     const filePath = path.join(
         __dirname,
         "public-download",
-        "ythub.1.4.1.apk"
+        "ythub.1.4.2.apk"
     );
 
-    res.download(filePath, "ythub.1.4.1.apk", (err) => {
+    res.download(filePath, "ythub.1.4.2.apk", (err) => {
 
         if (err) {
 
